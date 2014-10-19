0369abb63644232b66cf83a720a083c5301d8518
J <jy@jiangyandeMacBook-Pro.local>
2014-10-19 15:42:54 +0800

    Initial Commit

---
+++ WQZJ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WQZJ
+//
+//  Created by J on 14-10-19.
+//  Copyright (c) 2014年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WQZJ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WQZJ
+//
+//  Created by J on 14-10-19.
+//  Copyright (c) 2014年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
